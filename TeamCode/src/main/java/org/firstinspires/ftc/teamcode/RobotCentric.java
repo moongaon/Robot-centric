104a00ff86785e9bce8db1e99dbc66da85c1cc5d
moong <moongaon3@gmail.com>
2025-09-02 19:31:32 -0400

    initial project setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentric.java
@@ -1,6 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+@TeleOp
+public class RobotCentric extends LinearOpMode{
+    public  void runOpMode(){
 
-public class RobotCentric extends LinearOpMode {
-}
+
+        DcMotor TopLeftMotor = hardwareMap.get(DcMotor.class, "TopLeft Motor");//1
+        DcMotor TopRightMotor = hardwareMap.get(DcMotor.class, "TopRight Motor");//3
+        DcMotor BottomLeftMotor = hardwareMap.get(DcMotor.class, "BottomLeft Motor");//2
+        DcMotor BottomRightMotor = hardwareMap.get(DcMotor.class, "BottomRight Motor"); //0
+        TopLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        TopRightMotor.setDirection(DcMotor.Direction.FORWARD );
+        BottomLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        BottomRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        waitForStart();
+        while(opModeIsActive()){
+            Gamepad currentGamepad1 = new Gamepad();
+            double vertical = -gamepad1.left_stick_y;
+            double horizontal = gamepad1.left_stick_x;
+            double rotation = gamepad1.right_stick_x;
+            TopLeftMotor.setPower(vertical + horizontal + rotation);
+            BottomLeftMotor.setPower(vertical - horizontal + rotation);
+            TopRightMotor.setPower(vertical - horizontal - rotation);
+            BottomRightMotor.setPower(vertical + horizontal - rotation);
+           }
+        }
+    }
